050011ddc027fbbc9c9ee04ec60922384a8b1832
Bryce Eklund <bryce.eklund@gmail.com>
2019-11-30 13:11:03 -0800

    split string on separator

---
+++ string-splitter.js
@@ -0,0 +1,19 @@
+function splitString(str, sep, idx=0, arr=[], acc=[]) {
+    //base case
+    if (!str[idx]) {
+        return arr.push(acc.join(''))
+    }
+    //separator
+    if (str[idx] === sep) {
+        arr.push(acc.join(''))
+        return splitString(str, sep, idx + 1, arr)
+    }
+    //immediate result accumulator
+    acc.push(str[idx])
+    //recursive case
+    return splitString(str, sep, idx + 1, arr, acc)
+}
+
+splitString('i{love{to{eat{lemons', '{')
+
+//this one was fun. i just wrote a higher-order function?!?!
